117de64f9a812f1f6a9be0265adb11a44f85d1f0
hoangphuong <thongoc212249@gmail.com>
2014-01-30 15:21:35 +0700

    cm list item

--- anne-fashion.vn/protected/components/views/itemBox.php
+++ anne-fashion.vn/protected/components/views/itemBox.php
@@ -1,9 +1,9 @@
 <li class="col-md-4">
 	<div class="product-box">
 		<span class="sale_tag"></span>												
-		<a href="product_detail.html"><img alt="" src="http://www.captheme.com/bootstrap/sale_shop/images/project_img6.jpg?xx=2.jpg" class="img-responsive"></a><br/>
-		<a href="product_detail.html" class="title">Fusce id molestie massa</a><br/>
-		<a href="#" class="category">Phasellus consequat</a>
-		<p class="price">$341</p>
+		<a href="product_detail.html"><img alt="" src="https://d2isyty7gbnm74.cloudfront.net/z-aemSeUbT0OrSmU8vt2li8sR28=/646x646/d1g145x70srn7h.cloudfront.net/files/6f214f0121e80b146459805c4bddaaaab64776ea/original.jpeg" class="img-responsive"></a>
+		<a href="product_detail.html" class="title">Fusce id molestie massa</a>
+		<!--<a href="#" class="category">Phasellus consequat</a>-->
+		<p class="price">341.000 (VND)</p>
 	</div>
 </li>  
